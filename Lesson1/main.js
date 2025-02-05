8c25abcb896360fae128e4e0524af8b6bac4afc5
IvannaHrynkiv <grynkivivanna25@gmail.com>
2025-02-05 20:17:34 +0200

    коміт

---
+++ Lesson1/main.js
@@ -0,0 +1,42 @@
+let n = 'hello';
+console.log(n);
+let m = 'owu';
+console.log(m);
+let l = 'com';
+console.log(l);
+let d = 'ua';
+console.log(d);
+let k = 1;
+console.log(k);
+let g = 10;
+console.log(g);
+let h = -999;
+console.log(h);
+const PI =3.14;
+console.log(PI);
+let t = 2.7;
+console.log(t);
+let r = 16;
+console.log(r);
+let w = true;
+console.log(w);
+let v = false;
+console.log(v);
+
+
+let firstName ='Гриньків';
+console.log(firstName);
+let middleName = 'Іванна';
+console.log(middleName);
+let lastName = 'Олегівна';
+console.log(lastName);
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person);
+
+
+let a = 100;
+console.log(typeof a);
+let b ='100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
