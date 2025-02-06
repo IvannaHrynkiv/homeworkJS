0fa0154d14d82f80c8a8f1879e955b1c43b36e2f
IvannaHrynkiv <grynkivivanna25@gmail.com>
2025-02-06 21:15:10 +0200

    Lesson_2(1)

---
+++ Lesson2/main2.js
@@ -0,0 +1,162 @@
+let lessons = [1, 2, 3, 4, 5, 'homework_1', 'homework_2', 'homework_3', 'homework_4', 'homework_5'];
+console.log([0]);
+console.log([1]);
+console.log([2]);
+console.log([3]);
+console.log([4]);
+console.log([5]);
+console.log([6]);
+console.log([7]);
+console.log([8]);
+console.log([9]);
+console.log(lessons);
+
+
+
+let book1 = {
+    title:'Країна розваг',
+    pageCount:'367',
+    genre:'роман',
+}
+let book2 ={
+    title: 'Алхімік',
+    pageCount: '288',
+    genre: 'роман',
+}
+let book3 ={
+    title:'Переломний рік',
+    pageCount:'365',
+    genre:'психологія',
+}
+console.log(book1);
+console.log(book2);
+console.log(book3);
+
+
+let book4 ={
+    title:'Німа',
+    pageCount:'267',
+    genre:'роман',
+    authors:[{name:'Юлія Гук', age:'52'}],
+}
+let book5 ={
+    title:'Мазуревичі',
+    pageCount:'336',
+    genre:'історична проза',
+    authors: [{name: 'Дарина Гнатко', age: '42'}]
+}
+let book6 ={
+    title:'Тією горою є ви',
+    pageCount:'288',
+    genre:'саморозвиток',
+    authors:[{name:'Бріанна Вест', age:'33'}]
+}
+console.log(book4);
+console.log(book5);
+console.log(book6);
+
+
+
+let user1 ={
+    name:'Іван',
+    username:'Іван',
+    password:'1111',
+}
+let user2 ={
+    name:'Василь',
+    username: 'Василь',
+    password: '2222',
+}
+let user3 ={
+    name:'Ольга',
+    username:'Ольга',
+    password:'3333',
+}
+let user4 ={
+    name:'Іванна',
+    username:'Іванна',
+    password:'4444',
+}
+let user5 ={
+    name:'Юрій',
+    username:'Юрій',
+    password:'5555',
+}
+let user6 ={
+    name:'Марія',
+    username:'Марія',
+    password:'6666',
+}
+let user7 ={
+    name:'Андрій',
+    username:'Андрій',
+    password:'7777',
+}
+let user8 ={
+    name:'Яна',
+    username:'Яна',
+    password:'8888',
+}
+let user9 ={
+    name:'Микола',
+    username:'Микола',
+    password:'9999',
+}
+let user10 ={
+    name:'Катя',
+    username:'Катя',
+    password:'1234',
+}
+console.log(user1.password);
+console.log(user2.password);
+console.log(user3.password);
+console.log(user4.password);
+console.log(user5.password);
+console.log(user6.password);
+console.log(user7.password);
+console.log(user8.password);
+console.log(user9.password);
+console.log(user10.password);
+
+
+
+let weather =[
+    {day1:'monday',temp1:{
+        morning:-10,
+            dinner:0,
+            evening:-5
+        },
+        day2:'tuesday', temp2:{
+        morning: -8,
+            dinner:3,
+            evening: 0
+        },
+        day3:'wednesday', temp3:{
+        morning:0,
+            dinner:5,
+            evening:0
+        },
+        day4:'thursday', temp4:{
+        morning:1,
+            dinner:6,
+            evening:-1
+        },
+        day5:'friday', temp5:{
+        morning:0,
+            dinner:5,
+            evening:1
+        },
+        day6:'saturday', temp6:{
+        morning:1,
+            dinner:8,
+            evening:1
+        },
+        day7:'sunday', temp7:{
+        morning:2,
+            dinner:10,
+            evening:3
+        }
+    }
+]
+
+
